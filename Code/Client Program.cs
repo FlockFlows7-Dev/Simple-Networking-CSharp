7c2d78d8c36a33c3103739bb0b1976a2cadbc5f0
Matthew Clark <clarkmatthew707@gmail.com>
2025-02-13 13:21:41 -0600

    Add files via upload

---
+++ Code/Client Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Net.Sockets;
+using System.IO;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string serverAddress = "127.0.0.1"; 
+            int port = 8080;
+
+            try 
+            {
+                TcpClient client = new TcpClient(serverAddress, port);
+                NetworkStream stream = client.GetStream();
+                StreamWriter writer = new StreamWriter(stream) { AutoFlush = true };
+                StreamReader reader = new StreamReader(stream);
+
+                string serverMessage = reader.ReadLine();
+                Console.WriteLine("Message from server: " + serverMessage);
+
+
+                Console.Write("Enter message: ");
+                string clientMessage = Console.ReadLine();
+                writer.Flush();
+                writer.WriteLine(clientMessage);
+
+                Console.ReadLine();
+            }
+            catch(Exception ex) 
+            {
+                Console.WriteLine("Error: " + ex);
+            }
+        }
+    }
+}
---
+++ Code/Server Program.cs
@@ -0,0 +1,44 @@
+﻿
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.IO;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int port = 8080;
+            IPAddress iPAddress = IPAddress.Any;
+
+            TcpListener server = new TcpListener(iPAddress, port);
+            server.Start();
+
+            TcpClient client = server.AcceptTcpClient();
+            Console.WriteLine("Client Connected");
+
+            NetworkStream stream = client.GetStream();
+            StreamWriter writer = new StreamWriter(stream) { AutoFlush = true };
+            StreamReader reader = new StreamReader(stream);
+
+            string serverMessage = "Hello, Client";
+            writer.WriteLine(serverMessage);
+            writer.Flush();
+            Console.WriteLine("Message was sent to client: " + serverMessage);
+
+            string clientMessage;
+            clientMessage = reader.ReadLine();
+            Console.WriteLine("Client's message: " + clientMessage);
+            writer.WriteLine("Message received: " + clientMessage);
+
+
+
+            Console.ReadLine();
+            client.Close();
+            server.Stop();
+        }
+    }
+}
